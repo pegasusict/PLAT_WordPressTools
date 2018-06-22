1474adf92b88087565248f339d6e0bf900247eb7
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-22 12:12:23 +0200

    wip

--- wp_tools.sh
+++ wp_tools.sh
@@ -7,9 +7,9 @@
 go() { # pre-init
 	START_TIME=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
 	# Making sure this script is run by bash to prevent mishaps
-	if [ "$(ps -p "$$" -o comm=)" != "bash" ]	;	then bash "$0" "$@"	;	exit "$?"	;	fi
+	if [ "$(ps -p "$$" -o comm=)" != "bash" ] ; then bash "$0" "$@" ; exit "$?" ; fi
 	# Make sure only root can run this script
-	if [[ $EUID -ne 0 ]]	;	then echo "This script must be run as root"	;	exit 1	;	fi
+	if [[ $EUID -ne 0 ]] ; then echo "This script must be run as root" ; exit 1 ; fi
 }
 go $@
 init() {
